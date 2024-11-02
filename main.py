f1d7983cf5ca888aee9ed75da84c62d39d462dcf
ishal471 <ishalabhishek2000@gmail.com>
2024-11-02 14:42:33 -0500

    added port to main

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
+import os
 from resume_parser import iface  # Import Gradio interface from resume_parser
-iface.launch()
+iface.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 8000)))
